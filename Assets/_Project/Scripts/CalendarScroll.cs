2ae62b37ced35258f532c36ac39cc9477b401125
Ninellie <a.pawlow2009@ya.ru>
2024-10-11 23:25:01 +0300

    Added EndlessScroll to CalendarScroll.cs

--- Assets/_Project/Scripts/CalendarScroll.cs
+++ Assets/_Project/Scripts/CalendarScroll.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,40 +7,45 @@ public class CalendarScroll : MonoBehaviour
     [SerializeField] private int itemsCount;
     [SerializeField] private GameObject dateComponentPrefab;
     [SerializeField] private RectTransform content;
+    [SerializeField] private EndlessScroll endlessScroll;
     
     [Header("Item Properties")]
     [SerializeField] private CalendarDayColorPreset unselectedDayColors;
     [SerializeField] private CalendarDayColorPreset selectedDayColors;
-    
-    [SerializeField] private List<CalendarDay> items;
-    
+
     [SerializeField] private UnityEvent<DateTime> onDayClick;
     
     public CalendarDay SelectedDay { get; private set; }
     public UnityEvent<DateTime> OnDayClick => onDayClick;
     
-    private LinkedList<CalendarDay> _items;
+    private DateTime _selectedDayTime;
+    private DateTime _maxDateTime;
+    private DateTime _minDateTime;
 
     public void Initiate()
     {
-        _items = new LinkedList<CalendarDay>();
         var startDate = DateTime.Today.AddDays(Mathf.CeilToInt(-itemsCount / 2));
         CreateItems(startDate);
     }
 
+    private void OnEnable() => endlessScroll.onShift.AddListener(OnShift);
+
+    private void OnDisable() => endlessScroll.onShift.RemoveListener(OnShift);
+
     private void CreateItems(DateTime startDate)
     {
+        _minDateTime = startDate;
+        _selectedDayTime = DateTime.Today;
         for (var i = 0; i < itemsCount; i++)
         {
             var itemGameObject = Instantiate(dateComponentPrefab, content);
             var date = startDate.AddDays(i);
             
             var calendarDay = itemGameObject.GetComponent<CalendarDay>();
-            _items.AddLast(calendarDay);
 
             calendarDay.SetDate(date);
 
-            if (date == DateTime.Today)
+            if (date == _selectedDayTime)
             {
                 SelectDay(calendarDay);
                 onDayClick.Invoke(date);
@@ -54,6 +57,41 @@ public class CalendarScroll : MonoBehaviour
             
             calendarDay.OnClick.AddListener(() => onDayClick.Invoke(date));
             calendarDay.OnClick.AddListener(() => SelectDay(calendarDay));
+            _maxDateTime = date;
+        }
+    }
+    
+    private void OnShift(GameObject dayElement)
+    {
+        var day = dayElement.GetComponent<CalendarDay>();
+        
+        // Если сдвигаемый элемент выбран, раскрасить его как обычный день и убрать выбранный день, но не дату
+        if (day.DateTime == _selectedDayTime)
+        {
+            UnselectDay(day);
+            SelectedDay = null;
+        }
+        // Если произошёл шифт, то его дата или минимальна или максимальна
+        if (day.DateTime == _minDateTime)
+        {
+            // Элемент перешёл слево направо, значит нужно присвоить ему дату +1 день от максимальной
+            var dayAfterMaxDate = _maxDateTime.AddDays(1);
+            day.SetDate(dayAfterMaxDate);
+            _maxDateTime = dayAfterMaxDate;
+            _minDateTime = _minDateTime.AddDays(1);
+        }else if (day.DateTime == _maxDateTime) 
+        {
+            // Элемент перешёл справа налево, значит нужно присвоить ему дату -1 день от минимальной
+            var dayBeforeMinDate = _minDateTime.AddDays(-1);
+            day.SetDate(dayBeforeMinDate);
+            _minDateTime = dayBeforeMinDate;
+            _maxDateTime = _maxDateTime.AddDays(-1);
+        }
+
+        // Если элемент после того как был сдвинут стал той же даты что был выбранный - выбрать его визуально
+        if (day.DateTime == _selectedDayTime)
+        {
+            SelectDay(day);
         }
     }
     
@@ -74,6 +112,7 @@ public class CalendarScroll : MonoBehaviour
         day.SetDayColorGradient(selectedDayColors.DayText);
         
         SelectedDay = day;
+        _selectedDayTime = day.DateTime;
     }
 
     private void UnselectDay(CalendarDay day)
@@ -81,15 +120,5 @@ public class CalendarScroll : MonoBehaviour
         day.SetBackgroundColorGradient(unselectedDayColors.Background);
         day.SetDayOfWeekColorGradient(unselectedDayColors.DayOfWeek);
         day.SetDayColorGradient(unselectedDayColors.DayText);
-    }
-    
-    // Отображение для удобного просмотра списка в редактор
-    private void OnValidate()
-    {
-        if (_items == null)
-        {
-            return;
-        }
-        items = _items.ToList();
     }
 }
